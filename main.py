08079a50e3d1f62cc1dbdb2bac67d44b2e0c01d0
federico <federico.figaritomenotti@gmail.com>
2021-10-28 17:38:42 +0200

    implementation of classes is ok, lacks saving and maybe there is a bug to compute LAEO (which never shows up)

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pathlib import Path
+
 import main_classes as mc
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    v = mc.Video()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    saving_folder = Path('/home/federico/Videos')
+    current_video = mc.Video()
+    path = Path('/home/federico/Videos/Got01.mp4/ypr/176.json')
+    current_video.acquire_frame(path)
+    current_video.frame.observer.save_interactions(saving_folder)
+    print(f'ciao')
--- main_classes.py
+++ main_classes.py
@@ -5,6 +5,14 @@
 import pandas as pd
 from pathlib import Path
 
+from math import sin, cos
+import numpy as np
+
+import json_loader
+import laeo
+
+from collections.abc import Sequence
+
 
 class Vector_3D:  # or gaze_vector
     def __init__(self, yaw=None, pitch=None, roll=None):
@@ -34,8 +42,39 @@ class Vector_3D:  # or gaze_vector
         if int(self.yaw_u)==int(self.pitch_u)==int(self.roll_u)==0:
             self.__uncertainty_setter()
 
+    def __getitem__(self, i):
+        if i==0:
+            return self.yaw
+        if i==1:
+            return self.pitch
+        if i==2:
+            return self.roll
+        else:
+            raise IndexError
+
+    def __len__(self):
+        return int(3)
+
     def project_on_2D(self):
-        pass
+        """ Project yaw pitch roll on image plane. Result is NOT normalised.
+
+        :param yaw:
+        :param pitch:
+        :param roll:
+        :return:
+        """
+        pitch = self.pitch * np.pi / 180
+        yaw = -(self.yaw * np.pi / 180)
+        roll = self.roll * np.pi / 180
+
+        x3 = (sin(yaw))
+        y3 = (-cos(yaw) * sin(pitch))
+
+        # normalize the components
+        length = np.sqrt(x3**2 + y3**2)
+
+        # return [x3 / length, y3 / length]
+        return [x3, y3]
 
     def __uncertainty_setter(self, value=6.0):
         """It set the uncertainty when it is not provided at initialisation.
@@ -54,6 +93,12 @@ class Vector_3D:  # or gaze_vector
         self.pitch_u = value
         self.roll_u = value
 
+    def get_uncertainty(self):
+        return self.yaw_u, self.pitch_u, self.roll_u
+
+    def get_vector_components(self):
+        return self.yaw, self.pitch, self.roll
+
     def __repr__(self):
         return f'3D vector yaw={self.yaw}, pitch={self.yaw}, roll={self.yaw}'
 
@@ -61,9 +106,28 @@ class Vector_3D:  # or gaze_vector
         return self.yaw==other.yaw and self.pitch==other.pitch and self.roll==other.roll
 
 
-class Position_3D:
+class Position_3D(Sequence):
     def __init__(self, x, y, z=0):
         self.x, self.y, self.z = x, y, z
+
+    def __getitem__(self, i):
+        if i==0:
+            return self.x
+        if i==1:
+            return self.y
+        if i==2:
+            return self.z
+        else:
+            raise IndexError
+
+    def __len__(self):
+        if self.z==0:
+            return int(2)
+        else:
+            return int(3)
+
+    def __repr__(self):
+        return f'3D position x={self.x}, y={self.y}, z={self.z}'
 
 
 class Video:
@@ -74,28 +138,34 @@ class Video:
         self.history_of_people = self.HistoryPeople(self)
         self.initialise_tracker()  # don't know exactly
 
+    def start_analysis(self, video_path):
+        # video_path is a folder with json for each person
+        for frame in sorted(video_path.iterdir()):
+            self.acquire_frame(frame)
+
     def initialise_tracker(self):
         self.tracker = Video.Tracker()
         pass
 
-    def acquire_frame(self):
-        self.frame = self.Frame(self)
-        return self.frame
-
-    def save_history_current_frame(self):
-        pass
-
-
+    def acquire_frame(self, file):
+        if file.is_file():
+            data = json_loader.load_data(file)
+            self.frame = self.Frame(self, data)
+        else:
+            raise FileNotFoundError('.json file for this frame does not exist')
 
     class Tracker:
         def __init__(self):
             pass
 
         def update_tracker(self):
-            people_list = None
-            return people_list
+            self.people_list = None
+            return self.people_list
 
-    class HistoryPeople:
+        def person_is_new(self):
+            return True
+
+    class HistoryPeople:  # TODO implement
         def __init__(self, video_instance):
             self.video_instance = video_instance
             self.people_history = {}
@@ -115,59 +185,40 @@ class Video:
             destination_path = self.video_instance.saving_folder
 
     class Frame:
-        class Person:
-            _counter = 0
-
-            def __init__(self, yaw, pitxh, roll, tx, ty, tz=0):
-                Video.Frame.Person._counter += 1
-                self.id = Video.Frame.Person._counter
-                self.gaze = Vector_3D(yaw, pitxh, roll)
-                self.position = Position_3D(tx, ty, tz)
-
-            def assign_id(self, new_id):
-                self.id = new_id
-
-            def __repr__(self):
-                return f'Person id={id}'
-
-            def __eq__(self, other):
-                return self.id==other.id
-
-        class FrameObserver:
-            def __init__(self,frame_instance):
-                self.people = []  # list of obj of type Person
-                self.interaction = None
-                self.laeo = None
-                self.frame_instance=frame_instance
-                # self.frame_number = frame_number
-
-            def instantiate_people(self):
-                pass
-
-            def add_person(self, new_person):
-                # if tracker says it was a person already present, update id
-                if True:
-                    print(f'to implement!!!')
-                # check person is unique in frame
-                if new_person not in self.people:
-                    self.people.append(new_person)
-
-            def create_matrix(self):
-                index = [p.id for p in self.people]
-                matrix = pd.DataFrame(data=None, index=index, columns=index, dtype=float)
-                # matrix.at[0,0] get value at location [raw, coloumn], matrix.loc[5].at['B']
-            def compute_interactions(self):
-                pass
-            def compute_laeo(self):
-                pass
-            def save_interactions(self):
-                pass
         # start of the frame class
-        def __init__(self, video_instance):
+        def __init__(self, video_instance, data):
             self.video_instance = video_instance
             self.frame_number = Video.frame_counter
             Video.frame_counter += 1
-            self.observer = self.FrameObserver(self)
+            self.people = []  # list of obj of type Person
+            self.initialise_frame(data)
+            self.observer = self.FrameObserver(self)  # you should have all people fixed to call it
+
+        def initialise_frame(self, data):
+            self.pose_estimator()
+            self.instantiate_people(data)
+
+        def instantiate_people(self, data):
+            for p in data['people']:
+                x, y, z = None, None, None
+                if len(p['center_xy'])==2:
+                    x, y = p['center_xy'][0], p['center_xy'][1]
+                    z = 0
+                elif len(p['center_xy'])==3:
+                    x, y, z = p['center_xy'][0], p['center_xy'][1], p['center_xy'][2]
+                new_person = Video.Frame.Person(p['id_person'][0][0], [p['yaw'][0], p['yaw_u'][0]],
+                                                [p['pitch'][0], p['pitch_u'][0]],
+                                                [p['roll'][0], p['roll_u'][0]], x, y, z)
+                self.people.append(new_person)
+            pass
+
+        def add_person(self, new_person):
+
+            if self.video_instance.tracker.person_is_new():
+                if new_person not in self.people:  # check person is unique in frame
+                    self.people.append(new_person)
+            else:  # if tracker says it was a person already present, update id
+                print(f'to implement!!!')
 
         def pose_estimator(self):
             pass
@@ -175,12 +226,81 @@ class Video:
         def update_tracker(self):
             self.people_list = self.video_instance.tracker.update_tracker()
 
-
         def __repr__(self):
             return f'Frame number={self.frame_number}'
 
         def __eq__(self, other):
             return self.frame_number==other.frame_number
+
+        class Person:
+            _counter = 0
+
+            def __init__(self, person_id, yaw, pitch, roll, tx, ty, tz=0):
+                Video.Frame.Person._counter += 1
+                self.id = Video.Frame.Person._counter
+                self.position = Position_3D(tx, ty, tz)
+                self.gaze = Vector_3D(yaw, pitch, roll)
+                self.assign_id(person_id)
+                if tz==0:  # we are in 2D
+                    self.gaze.project_on_2D()
+
+            def assign_id(self, new_id):
+                self.id = new_id
+
+            def __repr__(self):
+                return f'Person id={self.id}'
+
+            def __eq__(self, other):
+                return self.id==other.id
+
+        class FrameObserver:
+            def __init__(self, frame_instance):
+
+                self.interaction = None
+                self.laeo = None
+                self.frame_instance = frame_instance
+                self.analyse_frame()
+                # self.frame_number = frame_number
+
+            def analyse_frame(self):
+                self.create_matrices()
+                self.compute_interactions()
+                self.compute_laeo()
+
+            def create_matrices(self):
+                index = [p.id for p in self.frame_instance.people]
+                self.interaction_matrix = pd.DataFrame(data=None, index=index, columns=index, dtype=float)
+                self.laeo_matrix = pd.DataFrame(data=None, index=index, columns=index, dtype=float)
+                # self.bool_matrix = pd.DataFrame(data=None, index=index, columns=index, dtype=float)
+                # matrix.at[0,0] get value at location [raw, coloumn], matrix.loc[5].at['B']
+
+            def compute_interactions(self):
+                for subject in self.frame_instance.people:
+                    for object in self.frame_instance.people:
+                        a, b, c = subject.gaze.get_uncertainty()
+                        # TODO clipping value to modify
+                        uncertainty = laeo.calculate_uncertainty(a, b, c, clipping_value=10)
+                        self.interaction_matrix.at[subject.id, object.id] = laeo.compute_interaction_cosine(
+                                subject.position, subject.gaze, uncertainty, object.position)
+
+            def compute_laeo(self):
+                for subject in self.frame_instance.people:
+                    for object in self.frame_instance.people:
+                        if np.allclose(self.interaction_matrix.at[subject.id, object.id],0) or np.allclose(self.interaction_matrix.at[object.id, subject.id],0):
+                            self.laeo_matrix.at[subject.id, object.id] = 0
+                        else:
+                            self.laeo_matrix.at[subject.id, object.id] = (self.interaction_matrix.at[
+                                                                              subject.id, object.id] +
+                                                                          self.interaction_matrix.at[
+                                                                              object.id, subject.id]) / 2
+
+
+            def save_interactions(self, path: Path):
+                if path.is_dir():
+                    destination = path / 'LAEO' / (str(self.frame_instance.frame_number) + '.csv')
+                    self.laeo_matrix.to_csv(destination, sep=',')
+                    destination = path / 'Interactions' / (str(self.frame_instance.frame_number) + '.csv')
+                    self.interaction_matrix.to_csv(destination, sep=',')
 
 
 class LAEO_matrix:
